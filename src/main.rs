7bb27a19cec587ea54ea606a71a25c04abb3a4ac
Pedro Henrique Martins Garcia <pedro.phmg@gmail.com>
2020-05-01 12:23:05 -0300

    Removendo mock temporário

--- src/main.rs
+++ src/main.rs
@@ -35,70 +35,6 @@ fn hello(name: String, age: u8) -> String {
     format!("Hello, {} year old named {}! Wellcome to LABS Backend!", age, name)
 }
 
-#[get("/")]
-fn amostra() -> &'static str {
-    "data = [{
-        id: 1,
-        Nome: 'TZ Eu Ag',
-        Matriz: 'TZO',
-        Dopante: 'Eu Ag',
-        Autor: 'Augusto',
-        Local: 'Gaveta',
-      }, {
-        id: 2,
-        Nome: 'TZ Eu',
-        Matriz: 'TZO',
-        Dopante: 'Eu',
-        Autor: 'Augusto',
-        Local: 'Gaveta',
-      }, {
-        id: 3,
-        Nome: 'TZN Eu',
-        Matriz: 'TZNO',
-        Dopante: 'Eu',
-        Autor: 'Augusto',
-        Local: 'Gaveta',
-      }, {
-        id: 4,
-        Nome: 'TZ Eu Ag',
-        Matriz: 'TZO',
-        Dopante: 'Eu Ag',
-        Autor: 'Augusto',
-        Local: 'Gaveta',
-      }, {
-        id: 5,
-        Nome: 'GP Eu',
-        Matriz: 'GPO',
-        Dopante: 'Eu',
-        Autor: 'Augusto',
-        Local: 'Gaveta',
-      }, {
-        id: 6,
-        Nome: 'GP Eu Ag',
-        Matriz: 'GPO',
-        Dopante: 'Eu Ag',
-        Autor: 'Augusto',
-        Local: 'Gaveta',
-      }, {
-        id: 7,
-        Nome: 'GP Eu Au',
-        Matriz: 'GPO',
-        Dopante: 'Eu Au',
-        Autor: 'Augusto',
-        Local: 'Gaveta',
-      }, {
-        id: 8,
-        Nome: 'TZA Eu',
-        Matriz: 'TZAO',
-        Dopante: 'Eu',
-        Autor: 'Augusto',
-        Local: 'Gaveta',
-      },
-    
-    ];"
-}
-
-
 
 #[get("/", format = "json")]
 fn amostra_ler(connection: db::Connection) -> Json<JsonValue> {
@@ -124,6 +60,7 @@ fn main() -> Result<(), Error> {
     .mount("/hello", routes![hello])
     .mount("/amostra", routes![amostra])
     .mount("/amostra/ler", routes![amostra_ler])
+    .mount("/amostra/add", routes![amostra_add])
     .manage(db::connect())
     .attach(cors)
     .launch();
